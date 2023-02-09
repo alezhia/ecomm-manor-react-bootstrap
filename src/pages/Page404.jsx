393caf197bea08cf5b4da3319a71c3f326ddba53
courfedra <franandres93@gmail.com>
2023-02-09 01:42:58 -0300

    add style

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,6 +1,8 @@
 const Page404=()=>{
     return(
-        <p>Error 404</p>
+        <div className="p-2 w-100 h-auto d-flex justify-content-center align-items-center" style={{backgroundColor:"#d69c4f"}}>
+            <img className="w-auto" src="./src/assets/page-not-found.png"/>
+        </div>
     )
 }
 export default Page404
