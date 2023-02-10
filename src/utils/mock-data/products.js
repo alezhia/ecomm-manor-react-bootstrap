a31dd40a05f16996e410d8161d9a0de946b55e08
courfedra <franandres93@gmail.com>
2023-02-10 12:53:50 -0300

    add products in mock-data

--- src/utils/mock-data/products.js
+++ src/utils/mock-data/products.js
@@ -31,4 +31,120 @@ export const products = [
         categories: [2, 5],
         priority: false
     },
+    {
+        id: '975770',
+        title: 'Sandalias Hombre Havaianas Top Athl 39/40',
+        description: 'Características principales: Gran cantidad de colores, comodidad, ligereza y durabilidad',
+        brand: 'Havaianas',
+        price: 44.90,
+        stock: 100,
+        images: [
+            'https://wongfood.vteximg.com.br/arquivos/ids/600581/Sandalias-Havaianas-Hombre-Aloha-Fc-39-40-1-347210145.jpg?v=638052541867600000',
+            'https://wongfood.vteximg.com.br/arquivos/ids/600468/Sandalias-Havaianas-Hombre-Aloha-Fc-39-40-2-347210145.jpg?v=638052537859770000',
+        ],
+        categories: [2, 4],
+        priority: false
+    },
+    {
+        id: '973045',
+        title: 'Pantuflas para Hombre Urb Talla 42 - 43',
+        description: 'Características principales: Comodidad, ligereza, de facil limpieza. En talla 42-43',
+        brand: 'Urb',
+        price: 17.49,
+        stock: 100,
+        images: [
+            'https://wongfood.vteximg.com.br/arquivos/ids/576155/Pantuflas-para-Hombre-Urb-Talla-44-45-1-345030573.jpg?v=637981184853600000',
+            'https://wongfood.vteximg.com.br/arquivos/ids/576154-750-750/Pantuflas-para-Hombre-Urb-Talla-42-43-1-345030572.jpg?v=637981184851570000',
+            'https://wongfood.vteximg.com.br/arquivos/ids/576088/Pantuflas-para-Hombre-Urb-Talla-44-45-3-345030573.jpg?v=637981179277270000',
+        ],
+        categories: [2, 4, 5],
+        priority: false
+    },
+    {
+        id: '532364',
+        title: 'Smartwatch KANJI Smw-002',
+        description: 'Diseño clásico, con funciones de control de sueño, mensajes, llamadas, podómetro, calendario y alarma.',
+        brand: 'Kanji',
+        price: 222.80,
+        stock: 100,
+        images: [
+            'https://static.cotodigital3.com.ar/sitios/fotos/full/00532300/00532364.jpg?3.0.150n',
+            'https://http2.mlstatic.com/D_NQ_NP_689911-MLA50876870268_072022-O.webp',
+            'https://http2.mlstatic.com/D_NQ_NP_677062-MLA50876870260_072022-O.webp',
+        ],
+        categories: [2, 3],
+        priority: false
+    },
+    {
+        id: '541638',
+        title: 'Smartwatch SAMSUNG Galaxy Watch4 46mm Negro',
+        description: 'Diseño clásico y premium con bisel giratorio. Sigue tus entrenamientos en tu muñeca',
+        brand: 'Samsung',
+        price: 2572.4,
+        stock: 100,
+        images: [
+            'https://static.cotodigital3.com.ar/sitios/fotos/full/00541600/00541638_g04.jpg',
+            'https://static.cotodigital3.com.ar/sitios/fotos/full/00541600/00541638_g03.jpg',
+            'https://static.cotodigital3.com.ar/sitios/fotos/full/00541600/00541638_g01.jpg',
+            'https://static.cotodigital3.com.ar/sitios/fotos/full/00541600/00541638_g05.jpg',
+        ],
+        categories: [1, 2, 3],
+        priority: false
+    },
+    {
+        id: '964417',
+        title: 'Jean Kansas Mujer Boom Bas Pitillo Celeste Os 30',
+        description: 'Material 85% Algodón - 13% Poliester - 2% Spandex; Tiro medio, fit pitillo, talla 30',
+        brand:"Kansas",
+        price: 59.99,
+        stock: 100,
+        images: [
+            'https://wongfood.vteximg.com.br/arquivos/ids/547275/Jean-Kansas-Mujer-Boom-Bas-Pitillo-Celeste-Os-30-1-308567364.jpg?v=637891218244530000',
+            'https://wongfood.vteximg.com.br/arquivos/ids/547071/Jean-Kansas-Mujer-Boom-Bas-Pitillo-Celeste-Os-30-5-308567364.jpg?v=637891201206300000',
+            'https://wongfood.vteximg.com.br/arquivos/ids/547070/Jean-Kansas-Mujer-Boom-Bas-Pitillo-Celeste-Os-30-4-308567364.jpg?v=637891201203970000',
+        ],
+        categories: [6, 7, 15],
+        priority: false
+    },
+    {
+        id: '974777',
+        title: 'Pantalón para Mujer Urb Tropic Talla S',
+        description: 'Talla S, suave, suelta y senacional para una noche con amigas',
+        brand:"Urb",
+        price: 34.99,
+        stock: 100,
+        images: [
+            'https://wongfood.vteximg.com.br/arquivos/ids/585804/Pantal-n-para-Mujer-Urb-Tropic-Talla-S-1-351632583.jpg?v=638007829772870000',
+            'https://wongfood.vteximg.com.br/arquivos/ids/585702/Pantal-n-para-Mujer-Urb-Tropic-Talla-S-2-351632583.jpg?v=638007827694470000',
+        ],
+        categories: [6, 15],
+        priority: false
+    },
+    {
+        id: '250920',
+        title: 'Cola Less Dama. Basic T5 43017ba',
+        description: 'Talla unica estilo cola less',
+        brand:"Syo",
+        price: 35.02,
+        stock: 100,
+        images: [
+            'https://static.cotodigital3.com.ar/sitios/fotos/full/00250900/00250920.jpg?3.0.150n',
+        ],
+        categories: [7, 8],
+        priority: false
+    },
+    {
+        id: '540599',
+        title: 'Traje De Baño Mujer Triangulo Lisa Rosa Talle L',
+        description: 'Traje de baño mujer triangulo lisa rosa talle L',
+        brand:"Top Design",
+        price: 70.89,
+        stock: 100,
+        images: [
+            'https://static.cotodigital3.com.ar/sitios/fotos/full/00540500/00540599.jpg?3.0.150n',
+        ],
+        categories: [6, 7, 9],
+        priority: false
+    },
 ]
+
