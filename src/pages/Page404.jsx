076f6bfb38dce48bebce76d966bc0b8335645c31
courfedra <franandres93@gmail.com>
2023-02-10 12:53:02 -0300

    Change h-auto to vh-100 in Page404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -2,7 +2,7 @@ import not_found from '../assets/page-not-found.png'
 
 const Page404=()=>{
     return(
-        <div className="p-2 w-100 h-auto d-flex justify-content-center align-items-center" style={{backgroundColor:"#d69c4f"}}>
+        <div className="p-2 w-100 vh-100 d-flex justify-content-center align-items-center" style={{backgroundColor:"#d69c4f"}}>
             <img className="w-auto" src={not_found}/>
         </div>
     )
