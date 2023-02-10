f3af16eedd47da53b94842e2f433111d98b77f5a
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-02-10 09:35:39 -0500

    feat: replace punctuation marks in slugIt function

--- src/utils/createSlug.js
+++ src/utils/createSlug.js
@@ -1,5 +1,5 @@
 export const slugIt = (text) =>
-    convertStrangeChars(text.toLowerCase().replaceAll(" ", "-"))
+    convertStrangeChars(text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?¿!¡]/g,"").replaceAll(" ", "-"))
 
 const convertStrangeChars = (text) =>
     text.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
