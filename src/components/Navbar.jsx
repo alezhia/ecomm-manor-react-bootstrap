3730c1deb5e62bc440b18599f3e6ef44eb171e0d
Sevindzh Shukurova <shukurova.s.n@gmail.com>
2023-02-15 16:59:48 -0500

    NavBar.css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,69 +1,45 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
 import logo from '../assets/manor-logotype.png'
-import {parentCategories} from '../utils/mock-data/parentCategories'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {Link} from 'react-router-dom';
+import { parentCategories, categories } from '../utils/mock-data';
+import '../styles/NavBar.css';
 
-
-const createLinks = (links) => 
-  Object.values(
-    links.reduce((acc, el) =>{
-      if(el.categorie <= "5"){
-        acc[el.id]={
-          ...el,
-          children:[]
-        };
-      }else {
-        acc[el.categorie].children.push(el)
-      }
-      return acc;
-    },{})
-
+const NavBar = () => {
+  return (
+    <Navbar className='navbar navbar-dark bg-dark' expand="lg">
+    <Container>
+      <Navbar.Brand href="#home"><img src={logo} alt=" Manor Logo " width="90px" /></Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          {
+            parentCategories.map(pCat => (
+              <NavDropdown key={pCat.id} title={pCat.title.toUpperCase()} id="collasible-nav-dropdown">
+                {
+                  categories.map(cat => pCat.categories.includes(cat.id) && <NavDropdown.Item key={cat.id} href={cat.slug}>{cat.title}</NavDropdown.Item>)
+                }
+              </NavDropdown>
+            ))
+          }
+        </Nav>
+        <Nav>
+        <Nav.Link as={Link} to="cart">CartWidget</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
+}
 
-const Navbar = () => {
-  // return (
-  //   <>
-  //     <img src={logo} alt="" width="10%" />
-  //     <hr />
-  //   </>
-  // )
-
-  const [toggle, setToggle] = useState ({});
-   const toggleSubmenu = (id) => (e) => {
-    e.preventDefault();
-
-    setToggle((ids) => ({
-      ...ids,
-      [id]:!ids[id]
-    }))
-   };
-
-  const cat  = createLinks(parentCategories).map((obj) =>{
-    return (
-      <ul className='navbar'>
-        <li categorieid={obj.categorie} key={obj.id}>
-          <Link to={obj.icon}>
-            <span className='link_name'>{obj.title}</span>
-            <i className='arrow' onClick={toggleSubmenu(obj.id)}>ver mas</i>
-          </Link>
-          {toggle[obj.id] && (
-            <ul className='sub-menu'>
-              {obj.children.map((nestedObj) => (
-                <li key={nestedObj.id}>
-                  <Link to={nestedObj.icon}>{nestedObj.title}</Link>
-                </li>
-              ))}
-
-            </ul>
-          )}
-
-        </li>
-
-      </ul>
-    )
-  })
-  return <div className='sidebar'>{cat}</div>
-};
+// function Priority(priority) {
+//   return (
+//     <div className={priority ? 'activo' : ''}/>
+    
+//   )
+// }
 
 
-export default Navbar
+export default NavBar
