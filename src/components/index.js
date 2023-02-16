bf62470109d3f70fcfecf4a945bb2482fcf3fdfd
courfedra <franandres93@gmail.com>
2023-02-16 14:14:13 -0300

    Change carousel to Carousel (Uppercase)

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,3 @@
 export { default as Breadcrumb } from "./Breadcrumb"
 export {default as Item} from "./Item"
-export {default as Carousel} from "./carousel"
+export {default as Carousel} from "./Carousel"
