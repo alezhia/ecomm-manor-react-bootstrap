168a3acaad44b49ec6ebd102273ac7e98f65e257
Maximiliano Safran <maxizero@gmail.com>
2023-03-03 19:15:19 -0300

    favs widget optimized

--- src/components/FavsWidget.jsx
+++ src/components/FavsWidget.jsx
@@ -1,32 +1,25 @@
 import "../styles/FavsWidget.css"
 import { MdFavoriteBorder, MdFavorite } from "react-icons/md"
-import { useContext } from "react"
+import { useState, useContext } from "react"
 import { FavsContext } from "../contexts/FavsContext"
 
 const FavsWidget = ({ item }) => {
-    const { isInFavs, addItem, removeItem } = useContext(FavsContext)
+    const [favIcon, setFavIcon] = useState([false])
+
+    const { favsItems, handlerFavs, isInFavs } = useContext(FavsContext)
+
+    console.log(favsItems)
 
     return (
         <div className="text-end me-2 mb-1 fs-3">
-            {isInFavs(item.id) ? (
-                <button
-                    onClick={() => {
-                        removeItem(item.id)
-                    }}
-                    className="bg-transparent p-0 border-0 favs-widget"
-                >
-                    <MdFavorite />
-                </button>
-            ) : (
-                <button
-                    onClick={() => {
-                        addItem(item)
-                    }}
-                    className="bg-transparent p-0 border-0 favs-widget"
-                >
-                    <MdFavoriteBorder />
-                </button>
-            )}
+            <button
+                onClick={() => {
+                    handlerFavs(item)
+                }}
+                className="bg-transparent p-0 border-0 favs-widget"
+            >
+                {isInFavs(item.id) ? <MdFavorite /> : <MdFavoriteBorder />}
+            </button>
         </div>
     )
 }
