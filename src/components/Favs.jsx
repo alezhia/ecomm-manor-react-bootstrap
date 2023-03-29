f0de62ac242433c840bae0acf44c5295e19efea2
Maximiliano Safran <maxizero@gmail.com>
2023-03-29 12:14:24 -0300

    adding styles Favs Component

--- src/components/Favs.jsx
+++ src/components/Favs.jsx
@@ -3,26 +3,34 @@ import { FavsContext } from "../contexts/FavsContext"
 import { useContext } from "react"
 import Image from "react-bootstrap/Image"
 import { FaTrashAlt } from "react-icons/fa"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 const Favs = ({ item }) => {
     const { removeItem } = useContext(FavsContext)
 
     return (
-        <div className="d-flex gap-5 my-3">
-            <Image
-                src={item.image}
-                alt={item.title}
-                style={{ width: "10rem" }}
-            />
-            <div className="description d-flex align-items-center">
-                <h4 className="description-title">{item.title}</h4>
-                <h4>{item.price}</h4>
-            </div>
-            <FaTrashAlt
-                className="trash h-auto"
-                onClick={() => removeItem(item.id)}
-            />
-        </div>
+        <Row className="d-flex align-items-center m-0">
+            <Col md={3} className="ps-0">
+                <Image
+                    src={item.image}
+                    alt={item.title}
+                    style={{ width: "10rem" }}
+                />
+            </Col>
+            <Col md={6}>
+                <h4 className="description-title mb-0">{item.title}</h4>
+            </Col>
+            <Col md={2}>
+                <h4 className="mb-0">{item.price}</h4>
+            </Col>
+            <Col md={1} className="pe-0">
+                <FaTrashAlt
+                    className="trash d-flex align-items-center"
+                    onClick={() => removeItem(item.id)}
+                />
+            </Col>
+        </Row>
     )
 }
 
--- src/containers/FavsContainer.jsx
+++ src/containers/FavsContainer.jsx
@@ -1,30 +1,36 @@
+import "../styles/FavsContainer.css"
 import { FavsContext } from "../contexts/FavsContext"
 import { useContext } from "react"
 import Favs from "../components/Favs"
 import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 const FavsContainer = () => {
     const { favsItems, clearFavs } = useContext(FavsContext)
 
     return (
         <Container className="d-flex flex-column">
-            <h1 className="text-center my-4">Favoritos</h1>
-            <button
-                onClick={clearFavs}
-                className="ms-auto me-5 bg-white fs-6 text-primary border-0 text-decoration-underline"
-            >
-                Eliminar Favoritos
-            </button>
-            <ul
-                style={{ border: "2px dashed #d69c4f" }}
-                className="d-flex flex-column p-0"
-            >
-                {favsItems.map((item) => (
-                    <li key={item.id} className="list-unstyled m-auto">
-                        <Favs item={item} />
-                    </li>
-                ))}
-            </ul>
+            <Row className="d-flex flex-row justify-content-center">
+                <Col lg={10}>
+                    <h1 className="text-center my-4">Favoritos</h1>
+                    <button
+                        onClick={clearFavs}
+                        className="d-block ms-auto mb-2 bg-white fs-6 text-primary border-0 text-decoration-underline"
+                    >
+                        Eliminar Favoritos
+                    </button>
+                    <ul className="p-3" style={{ border: "2px solid #d69c4f" }}>
+                        <Container className="fav-item d-flex flex-column gap-3 p-0">
+                            {favsItems.map((item) => (
+                                <li key={item.id} className="list-unstyled">
+                                    <Favs item={item} />
+                                </li>
+                            ))}
+                        </Container>
+                    </ul>
+                </Col>
+            </Row>
         </Container>
     )
 }
